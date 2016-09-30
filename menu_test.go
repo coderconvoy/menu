8f41781fd4d3ad9ed1ec6961e096974354995781
coderconvoy <thebeaky@psmail.net>
2016-09-30 11:05:52 +0100

    Initial Setup

---
+++ menu_test.go
@@ -0,0 +1,36 @@
+package menu
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+)
+
+func Test_Stuff(t *testing.T) {
+	ar := []string{"Hello", "{", "poo:pee", "Grow:Gree", "No", "}", "Goodbye:adios"}
+
+	m, err := NewMenu(ar)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(m)
+	r, _ := json.Marshal(m.Children)
+	fmt.Println(string(r))
+
+}
+
+func Test_fails(t *testing.T) {
+	ar := [][]string{
+		[]string{"{"},
+		[]string{"hello,", "{", "{"},
+	}
+	for _, v := range ar {
+		r, err := NewMenu(v)
+		fmt.Println(r)
+		if err == nil {
+			t.Fail()
+		}
+	}
+}
